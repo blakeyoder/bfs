e16c09a0330b0403c7254aae09905b7ba050387b
blakeyoder <yoderblake@gmail.com>
2015-12-29 22:43:42 -0500

    Parkers push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # before_action :check_age
+  # def check_age
+  #   @age_verify = cookies[:age]
+  #   if @age_verify.blank?
+  #     redirect_to root_url
+  #   end
+  # end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   layout false, only: [:landing]
+  skip_before_action :check_age, only: [:landing]
 
   def landing
   end
