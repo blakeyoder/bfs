aff590a6dc63d58308accd5c5665890d623ba65a
Blake Yoder <yoderblake@gmail.com>
2016-02-01 22:07:41 -0500

    Finished up related beers module waiting on styles for it

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -9,7 +9,7 @@ class BeersController < ApplicationController
 
   def show
     @beer = Beer.find(params[:id])
-    @related_beers = Beer.related_beers(@beer)
+    @related_beers = related_beer(@beer)
   end
 
   def create
@@ -23,5 +23,9 @@ class BeersController < ApplicationController
 
   def beer_params
     params.require(:beer).permit(:name, :style, :abv, :ibu, :color, :description)
+  end
+
+  def related_beer(beer)
+    Beer.where("style LIKE (?) and id <> (?)", beer.style, beer.id).take(5)
   end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,7 +2,4 @@ class Beer < ActiveRecord::Base
   has_many :images, :autosave => true
   accepts_nested_attributes_for :images #this is required for formtastic in active admin
 
-  def self.related_beers(beer)
-    Beer.where("style LIKE (?)", beer.style)
-  end
 end
