3cb2fddb7af37b27ab6cefb184b360879075493b
Blake Yoder <yoderblake@gmail.com>
2016-02-16 22:24:16 -0500

    Migrating to instagrams api

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,6 @@
+require 'instagram'
+
+Instagram.configure do |config|
+  config.client_id = "c5fdc96b78c846f5bbe30ac29ddd79e2"
+  config.access_token = "39375607.c5fdc96.7319267f6fea4e8da3e7425f9bc4a8c6"
+end
